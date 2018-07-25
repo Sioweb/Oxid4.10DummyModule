187073c08416cc5382a570a124f021adc45702c1
Sascha Weidner <support@sioweb.de>
2018-07-25 10:35:15 +0200

    Create SiowebDummy_Utilsview.php

---
+++ siowebDummyModule/core/SiowebDummy_Utilsview.php
@@ -0,0 +1,10 @@
+<?php
+
+class SiowebDummy_Utilsview extends SiowebDummy_Utilsview_parent
+{
+    protected function _fillCommonSmartyProperties($oSmarty)
+    {
+        parent::_fillCommonSmartyProperties($oSmarty);
+        array_unshift($oSmarty->plugins_dir, oxRegistry::getConfig()->getModulesDir() . "/sioweb/siowebDummyModule/smarty/");
+    }
+}
