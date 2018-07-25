63bd5ca452cd5ed054191f6b558b4ea5ffd07c1e
Sascha Weidner <support@sioweb.de>
2018-07-25 13:49:15 +0200

    Create SiowebDummyController.php

---
+++ siowebDummyModule/application/controllers/SiowebDummyController.php
@@ -0,0 +1,14 @@
+<?php
+
+class SiowebDummyController extends oxUBase
+{
+    protected $_sThisTemplate = 'sioweb_dummy_controller.tpl';
+    protected $_oArticle = null;
+    protected $_oDetails = null;
+
+    public function render()
+    {
+        // Do some Controller shit
+        return parent::render();
+    }
+}
