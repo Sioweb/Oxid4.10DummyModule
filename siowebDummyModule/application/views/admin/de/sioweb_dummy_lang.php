582d3a5d6f89a5d8b1285d9872baf4a3029d9e67
Sascha Weidner <support@sioweb.de>
2018-07-25 11:08:32 +0200

    Create sioweb_dummy_lang.php

---
+++ siowebDummyModule/application/views/admin/de/sioweb_dummy_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$sLangName = 'Deutsch';
+$aLang = array(
+    'charset'                               => 'UTF-8',
+    'WHAT_EVER_U_NEED'                      => 'Was auch immer du übersetzen willst',
+);
